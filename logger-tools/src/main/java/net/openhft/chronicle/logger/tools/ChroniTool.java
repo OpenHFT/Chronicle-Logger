6b8cd4dfa061c8198152b25d68cb2cf49bddd2a0
lburgazzoli <lburgazzoli@gmail.com>
2015-02-09 14:10:51 +0100

    Improve tools

--- logger-tools/src/main/java/net/openhft/chronicle/logger/tools/ChroniTool.java
+++ logger-tools/src/main/java/net/openhft/chronicle/logger/tools/ChroniTool.java
@@ -27,6 +27,7 @@ import net.openhft.lang.model.constraints.NotNull;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Objects;
 
 public final class ChroniTool {
 
@@ -55,16 +56,22 @@ public final class ChroniTool {
     // *************************************************************************
 
     public static final ChronicleLogReader READER_BINARY = new BinaryProcessor() {
+        private StringBuilder sb = new StringBuilder();
+
         @Override
         public void process(final ChronicleLogEvent event) {
-            System.out.println(asString(event));
+            sb.setLength(0);
+            System.out.println(asString(event, sb));
         }
     };
 
     public static final ChronicleLogReader READER_TEXT = new TextProcessor() {
+        private StringBuilder sb = new StringBuilder();
+
         @Override
         public void process(final ChronicleLogEvent event) {
-            System.out.println(asString(event));
+            sb.setLength(0);
+            System.out.println(asString(event, sb));
         }
     };
 
@@ -72,27 +79,58 @@ public final class ChroniTool {
     //
     // *************************************************************************
 
-    public static ChronicleLogReader reader(final ChronicleLogProcessor processor) {
-        return new BinaryProcessor() {
-            @Override
-            public void process(final ChronicleLogEvent event) {
-                processor.process(event);
+    public static StringBuilder asString(final ChronicleLogEvent event, final StringBuilder sb) {
+        sb.append(DF.format(event.getTimeStamp()));
+        sb.append("|");
+        sb.append(event.getLevel().toString());
+        sb.append("|");
+        sb.append(event.getThreadName());
+        sb.append("|");
+        sb.append(event.getLoggerName());
+        sb.append("|");
+        sb.append(event.getMessage());
+
+        Object[] args = event.getArgumentArray();
+        if(args != null) {
+            sb.append("|args {");
+            for(int i=0; i<args.length; i++) {
+                sb.append(Objects.toString(args[i]));
+                if(i != args.length - 1){
+                    sb.append(", ");
+                }
             }
-        };
+
+            sb.append("}");
+        }
+
+        final Throwable th = event.getThrowable();
+        if(th != null) {
+            sb.append("|exception: ");
+            sb.append(th.getMessage());
+
+            StackTraceElement[] elements = th.getStackTrace();
+            for(int i=0; i<elements.length; i++) {
+                sb.append("\n\tat ");
+                sb.append(elements[i].toString());
+            }
+
+            final Throwable cause = th.getCause();
+            if(cause != null) {
+                sb.append("\n\tCaused by: ");
+                sb.append(cause.getMessage());
+
+                elements = cause.getStackTrace();
+                for(int i=0; i<elements.length; i++) {
+                    sb.append("\n\t\tat");
+                    sb.append(elements[i].toString());
+                }
+            }
+        }
+
+        return sb;
     }
 
-    public static String asString(final ChronicleLogEvent event) {
-        if(!event.hasArguments()) {
-            return String.format("%s|%s|%s|%s|%s\n",
-                DF.format(event.getTimeStamp()),
-                event.getLevel().toString(),
-                event.getThreadName(),
-                event.getLoggerName(),
-                event.getMessage());
-        } else {
-            return "";
-        }
-    }
+
 
     // *************************************************************************
     //
