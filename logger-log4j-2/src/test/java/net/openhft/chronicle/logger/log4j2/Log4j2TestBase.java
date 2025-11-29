7273e4e2a7fe7462ae31ac8f6ce43210af5f0e1d
Peter Lawrey <peter.lawrey@chronicle.software>
2025-11-29 16:11:35 +0000

    Refactor Log4j2 tests to replace SLF4J with Log4j for improved logging consistency

--- logger-log4j-2/src/test/java/net/openhft/chronicle/logger/log4j2/Log4j2TestBase.java
+++ logger-log4j-2/src/test/java/net/openhft/chronicle/logger/log4j2/Log4j2TestBase.java
@@ -4,8 +4,8 @@
 package net.openhft.chronicle.logger.log4j2;
 
 import net.openhft.chronicle.logger.ChronicleLogLevel;
+import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-import org.slf4j.Logger;
 
 import java.io.File;
 
